2b2eea8f9638f0ec2ccf1a5c93a4950e63c2c863
Izalhadji31 <ryzalhadji@gmail.com>
2024-10-18 12:43:00 +0700

    first commit project layout

---
+++ app/src/main/java/com/example/layout/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.layout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.layout.ui.theme.LayoutTheme
+import java.time.format.TextStyle
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LayoutTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    BasicCompose(
+                        izal = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+@Preview (showBackground = true)
+@Composable
+fun BasicCompose(izal: Modifier = Modifier){
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        ){
+        Text(
+            "Indonesia",
+            modifier = izal,
+            fontSize = 70.sp,
+            color = Color.Blue,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            "Versus",
+            style = androidx.compose.ui.text.TextStyle(
+                fontSize = 50.sp,
+                fontStyle = FontStyle.Italic,
+                color = Color.Red,
+            )
+
+        )
+        Image(painter = painterResource(id = R.drawable.download),
+            contentDescription = null,
+            modifier = izal.size(200.dp)
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LayoutTheme {
+        Greeting("Android")
+    }
+}
